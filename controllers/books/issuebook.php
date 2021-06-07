9f927addb5ed2a7c41aea71872ce806c7c47e3d1
Aman Sharma <amansharma3009@hotmail.com>
2021-06-07 18:30:46 +0530

    Issue Book One Per User Resolved

--- controllers/books/issuebook.php
+++ controllers/books/issuebook.php
@@ -5,8 +5,23 @@ if(isset($_POST['issue'])){
        $u_id=$_SESSION['uid'];
         $b_id=$_POST['bid'];
         $issue=$_POST['action'];
-        App::get('Books')->IssueBook($u_id,$b_id,$issue);
+        if($_POST['action'] =='reading'){
+            $data=App::get('Users')->Selectcondition($u_id,$issue);
+            $data->execute();
+            $has = $data->rowCount();
+            if($has>0){
+                echo 'You have to finish the book you are reading in order to issue book';
+            }elseif($has==0){
+            App::get('Books')->IssueBook($u_id,$b_id,$issue);
         header("location:/booklist");
+            }
+        }
+        if($_POST['action'] =='wishlisted'){
+            App::get('Books')->IssueBook($u_id,$b_id,$issue);
+        header("location:/booklist");
+            
+        }
+        
     }
 }
 ?>
